4be8cfcd96420975197b00b6714915fd05e1c357
Scott R. Santos <santos@auburn.edu>
2017-05-22 09:13:24 -0500

    Tweaked theme

--- luakit/.config/luakit/theme.lua
+++ luakit/.config/luakit/theme.lua
@@ -6,14 +6,14 @@ local theme = {}
 
 -- Default settings
 theme.font = "16px Roboto Condensed"
-theme.fg   = "#fff"
-theme.bg   = "#000"
+theme.fg   = "#000"
+theme.bg   = "#D3D3D3"
 
 -- Genaral colours
 theme.success_fg = "#0f0"
-theme.loaded_fg  = "#33AADD"
-theme.error_fg = "#FFF"
-theme.error_bg = "#F00"
+theme.loaded_fg  = "#0000CC"
+theme.error_fg   = "#FFF"
+theme.error_bg   = "#F00"
 
 -- Warning colours
 theme.warning_fg = "#F00"
@@ -34,10 +34,10 @@ theme.menu_secondary_title_fg   = "#666"
 
 theme.menu_disabled_fg = "#999"
 theme.menu_disabled_bg = theme.menu_bg
-theme.menu_enabled_fg = theme.menu_fg
-theme.menu_enabled_bg = theme.menu_bg
-theme.menu_active_fg = "#060"
-theme.menu_active_bg = theme.menu_bg
+theme.menu_enabled_fg  = theme.menu_fg
+theme.menu_enabled_bg  = theme.menu_bg
+theme.menu_active_fg   = "#060"
+theme.menu_active_bg   = theme.menu_bg
 
 -- Proxy manager
 theme.proxy_active_menu_fg      = '#000'
@@ -46,8 +46,8 @@ theme.proxy_inactive_menu_fg    = '#888'
 theme.proxy_inactive_menu_bg    = '#FFF'
 
 -- Statusbar specific
-theme.sbar_fg         = "#fff"
-theme.sbar_bg         = "#000"
+theme.sbar_fg         = theme.fg
+theme.sbar_bg         = theme.bg
 
 -- Downloadbar specific
 theme.dbar_fg         = "#fff"
@@ -55,8 +55,8 @@ theme.dbar_bg         = "#000"
 theme.dbar_error_fg   = "#F00"
 
 -- Input bar specific
-theme.ibar_fg           = "#000"
-theme.ibar_bg           = "rgba(00,00,00,0.20)"
+theme.ibar_fg           = "#fff"
+theme.ibar_bg           = "#000"
 
 -- Tab label
 theme.tab_fg            = "#888"
@@ -70,11 +70,11 @@ theme.loading_fg        = "#33AADD"
 theme.loading_bg        = "#fff"
 
 theme.selected_private_tab_bg = "#3d295b"
-theme.private_tab_bg    = "#22254a"
+theme.private_tab_bg          = "#22254a"
 
 -- Trusted/untrusted ssl colours
-theme.trust_fg          = "#0F0"
-theme.notrust_fg        = "#F00"
+theme.trust_fg          = "#009900"
+theme.notrust_fg        = "#990000"
 
 -- General colour pairings
 theme.ok = { fg = "#000", bg = "#FFF" }
